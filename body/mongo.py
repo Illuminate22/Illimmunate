55a6c7d321d024df977313636be6fc595b8c3b79
SaiMadhavanG <g.saimadhavan@gmail.com>
2022-02-12 10:39:47 +0530

    mongo file updated

--- body/mongo.py
+++ body/mongo.py
@@ -6,4 +6,5 @@ ca = certifi.where()
 connection = pymongo.MongoClient("mongodb+srv://user1:honeycake123@cluster0.zd1jh.mongodb.net/database_main?retryWrites=true&w=majority", tlsCAFile=ca)
 db = connection["database_main"]
 childcol = db["child"]
+vaxcol = db["vaccine"]
 
